b4d2c89a181f7520d6e1b48c68b387b93377e040
adnan-md-5491 <adnan.mohammed@learnmoretechnologies.in>
2026-09-03 11:38:27 +0530

    done

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World jj!';
+    return 'Hello World jj! i have deplpoyed';
   }
 }
